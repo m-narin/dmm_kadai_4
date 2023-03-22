3ea287ccdf8944a1d0cfb5e3574c532ac3f7a2f2
mando0520 <y2pgftpe@gmail.com>
2023-03-22 16:08:19 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to:'homes#top'
+
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
+
+  get "homes/about" => "homes#about", as: :about
+end
