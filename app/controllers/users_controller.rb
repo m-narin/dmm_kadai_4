a47e33eb8012d77f311fe92efdae79fc407ddd96
mando0520 <y2pgftpe@gmail.com>
2023-03-23 15:47:03 +0000

    ユーザー詳細ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images.page(params[:page])
+    @book = Book.new
   end
 
   def edit
